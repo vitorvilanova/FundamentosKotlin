cbf5b1ce527765cc405d497b979f09070bb39ead
vitorvilanova <vitorvilanovapinto@gmail.com>
2022-02-13 09:00:13 -0300

    Fundamentos

--- src/fundamentos/ConversaoNumeroString.kt
+++ src/fundamentos/ConversaoNumeroString.kt
@@ -2,11 +2,13 @@ package fundamentos
 
 fun main(args: Array<String>) {
     val a = 1
+
     // Número para String
     println(a.toString() + 1)
+
     // String para Número
     println("1.9".toDouble() + 3)
     println("Teste".toIntOrNull())
-    println("Teste".toIntOrNull() ?: 0)
+    println("Teste".toIntOrNull() ?: 1993)
     println("1".toInt() + 3)
 }
