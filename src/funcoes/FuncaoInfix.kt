ef4d93932aa347a44f7ab083d7296794701c4273
vitorvilanova <vitorvilanovapinto@gmail.com>
2022-02-13 12:14:41 -0300

    Funcoes

--- src/funcoes/FuncaoInfix.kt
+++ src/funcoes/FuncaoInfix.kt
@@ -9,5 +9,6 @@ fun main(args: Array<String>) {
     val p2 = Produto(preco = 3.39, nome =  "Borracha")
     println(p1 maisCaroQue  p2)
     //chamando modo tradicional infix
+    // 1 + 1 é um modelo de infix (+)
     println(p2.maisCaroQue(p1))
 }
