7a72ceffa26685baf84d4b63513018adb553d004
vitorvilanova <vitorvilanovapinto@gmail.com>
2022-02-13 08:30:09 -0300

    Exercicios de controles

--- src/fundamentos/operadores/Unarios.kt
+++ src/fundamentos/operadores/Unarios.kt
@@ -7,7 +7,15 @@ fun main(args: Array<String>) {
     println(num1)
     --num1
     println(num1)
-    // Incremento e decremento
-    println(++num1 == num2--)
-    println(num1 == num2)
+
+    /*
+    Incremento e decremento
+    pre fixado  ++num1 ele incrementa antes da execução
+    pos fixado num2-- ele decrementa apos a execução
+     */
+
+    println(++num1 == num2--)   //Resultado sera true; Porque os dois numeros serão 2
+
+
+    println(num1 == num2) // Resultado sera false, porque ja executou e alterou as variaveis
 }
