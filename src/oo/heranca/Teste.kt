f0add58f0af2711daef1e0cd8f997509897c5b51
vitorvilanova <vitorvilanovapinto@gmail.com>
2022-02-16 08:11:27 -0300

    Orientacao Objeto Heranca

---
+++ src/oo/heranca/Teste.kt
@@ -0,0 +1,13 @@
+package oo.heranca
+
+fun main(args: Array<String>) {
+    val carro = Ferrari()
+    carro.acelerar()
+    println(carro)
+    carro.ligarTurbo()
+    carro.acelerar()
+    println(carro)
+    carro.frear()
+    carro.frear()
+    println(carro)
+}
